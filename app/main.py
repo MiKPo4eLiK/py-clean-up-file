cbdb4b63fb5369645371aa46789616bc5ea22c08
MiKPo4eLiK <stassojcuk@gmail.com>
2025-05-20 20:02:41 +0300

    well

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(self, exc_type: str, exc_value: str, traceback: str) -> bool:
+        if os.path.exists(self.filename):
+            return False
+
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
